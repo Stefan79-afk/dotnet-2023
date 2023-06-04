61470dc7b66ab1cd122504bc68af6704a1a3dc76
Cosmin - Stefan Negureanu <epsilon.number79@gmail.com>
2023-06-04 15:23:19 +0300

    create MapProperty.cs enum file for map features like highways, landuses and so on

---
+++ Common/MemoryMappedTypes/MapProperty.cs
@@ -0,0 +1,30 @@
+﻿namespace Mapster.Common.MemoryMappedTypes;
+
+public enum MapProperty
+{
+    Unknown = 0,
+    Waterway = 1,
+    PlaceName = 2,
+
+    Highway = 3,
+    HighwayMotorway = 4,
+    HighwayTrunk = 5,
+    HighwayPrimary = 6,
+    HighwaySecondary = 7,
+    HighwayTertiary = 8,
+    HighwayResidential = 9,
+
+    Railway = 10,
+
+    Border = 11,
+
+    Building = 12,
+
+    LanduseNatural = 13,
+    LanduseForest = 14,
+    LandusePlain = 15,
+    LanduseNaturalMountains = 16,
+    LanduseNaturalDesert = 17,
+    LanduseNaturalWater = 18,
+    LanduseResidential = 19
+}
